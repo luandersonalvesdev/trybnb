15ff58b3296a2486069cec5b63c6230a384da96c
Luanderson Alves <luaoderson@gmail.com>
2024-01-12 17:58:46 -0300

    feat: buttons change fragments

--- app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
@@ -1,13 +1,48 @@
 package com.betrybe.trybnb.ui.views.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.betrybe.trybnb.R
+import com.betrybe.trybnb.databinding.ActivityMainBinding
+import com.betrybe.trybnb.ui.views.fragments.CreateReservationFragment
+import com.betrybe.trybnb.ui.views.fragments.ProfileFragment
+import com.betrybe.trybnb.ui.views.fragments.ReservationFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.navigationBottomView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.reservation_menu_item -> {
+                    switchFragment(ReservationFragment())
+                    true
+                }
+                R.id.create_reservation_menu_item -> {
+                    switchFragment(CreateReservationFragment())
+                    true
+                }
+                R.id.profile_menu_tem -> {
+                    switchFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun switchFragment(newFragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.mainFragmentContainer.id, newFragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.betrybe.trybnb.ui.views.fragments
+
+import androidx.fragment.app.Fragment
+import com.betrybe.trybnb.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
